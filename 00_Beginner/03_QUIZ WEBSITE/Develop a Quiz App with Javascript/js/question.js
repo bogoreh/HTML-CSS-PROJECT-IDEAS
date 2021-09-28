ba2684300f75d3ca2e92c22a298892234e883899
bogoreh <76124689+bogoreh@users.noreply.github.com>
2021-09-28 06:09:03 +0300

    The first commit

---
+++ 00_Beginner/03_QUIZ WEBSITE/Develop a Quiz App with Javascript/js/question.js
@@ -0,0 +1,13 @@
+/*
+Author: Abdibogoreh
+*/
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+}
---
+++ 00_Beginner/03_QUIZ WEBSITE/Develop a Quiz App with Javascript/js/quiz.js
@@ -0,0 +1,25 @@
+/*
+Author: Abdibogoreh
+*/
+
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function () {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function (answer) {
+    this.questionIndex++;
+    if (this.getQuestionIndex().correctAnswer(answer)) {
+        this.score++;
+    }
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function () {
+    return this.questionIndex === this.questions.length;
+}
